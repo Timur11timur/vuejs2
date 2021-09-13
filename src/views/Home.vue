478f14e429476ecbbb29d0dd34e438a330b3bede
Timur11timur <timur__timur@mail.ru>
2021-09-14 00:21:22 +0300

    Add vue router

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis cupiditate ipsum laudantium libero officiis possimus quia, sit veritatis vero voluptatibus.</p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
